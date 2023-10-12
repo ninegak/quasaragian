0c86ef966c702fd5ae8cad0dfd63164b428265dd
ninegak <zxce2546@gmail.com>
2023-10-12 21:30:20 +0700

    messing with majestic

---
+++ src/pages/InputPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="จักรพันธุ์" v-model="firstName" />
+      <input type="text" id="แฉล้มนงนุช (6504101311)" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'Chakkaphan Chalamnongnuch',
+      firstName: '',
+      secondName: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
